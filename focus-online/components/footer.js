7365af44948b2dcfcc4d00e01a528c409ddc097c
troyemmerson <troyemmerson16@hotmail.com>
2025-02-25 14:58:44 +0200

    Header & Footer components

---
+++ focus-online/components/footer.js
@@ -0,0 +1,10 @@
+import { gql } from "@apollo/client";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer>
+        Footer section
+    </footer>
+  );
+}
---
+++ focus-online/components/header.js
@@ -0,0 +1,10 @@
+import { gql } from "@apollo/client";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header>
+        Header section
+    </header>
+  );
+}
