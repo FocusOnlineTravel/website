ce43b82f782c4ab0191f5a9a126e684a0f6c3560
troyemmerson <troyemmerson16@hotmail.com>
2025-02-26 09:03:45 +0200

    Generate types and Tailwind setup

--- focus-online/pages/_app.js
+++ focus-online/pages/_app.js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { FaustProvider } from "@faustwp/core";
 import "../faust.config";
 import "@faustwp/core/dist/css/toolbar.css";
+import '@/styles/globals.css';
  
 export default function App({ Component, pageProps }) {
 	const router = useRouter();
