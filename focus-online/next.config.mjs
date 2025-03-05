630e551917078d2f2817baafeb520557f0089b3f
troyemmerson <troyemmerson16@hotmail.com>
2025-03-05 16:32:35 +0200

    Custom logo and Primary Header

--- focus-online/next.config.mjs
+++ focus-online/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*", // Allow images from all domains
+      },
+    ],
+  },
 };
 
 export default nextConfig;
